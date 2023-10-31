a4c6b041971af1e874b8d79ddcf5219aaca4332a
Kaych3e <nkaduba@gmail.com>
2023-10-31 14:20:25 -0700

    task5

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
   plugins: [
